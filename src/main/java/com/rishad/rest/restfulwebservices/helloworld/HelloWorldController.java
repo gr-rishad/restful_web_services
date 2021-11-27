335d9b31de42311fafb9208fd6e932c2b76ebc28
Golam Rabbani Rishad <33056647+gr-rishad@users.noreply.github.com>
2021-11-27 23:32:07 +0600

    Implementting Support xml

--- src/main/java/com/rishad/rest/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rishad/rest/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,10 +1,17 @@
 package com.rishad.rest.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 // controller -> handle http request
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     // GET
     //URI - /hello-world
@@ -18,6 +25,12 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorld helloWorldBean() {
         return new HelloWorld("Hello World");
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, "DEFAULT MESSAGE", locale);
+        //return new HelloWorld("Hello World");
     }
 
     @GetMapping("/hello-world/path-variable/{name}")
--- src/main/java/com/rishad/rest/restfulwebservices/user/UserController.java
+++ src/main/java/com/rishad/rest/restfulwebservices/user/UserController.java
@@ -3,6 +3,11 @@ package com.rishad.rest.restfulwebservices.user;
 import com.rishad.rest.restfulwebservices.exception.CustomizeExceptionHandler;
 import com.rishad.rest.restfulwebservices.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,20 +32,21 @@ public class UserController extends CustomizeExceptionHandler {
 
     //retrieveUser(int id)
     @GetMapping("/users/{id}")
-    public User retrieveUserById(@PathVariable int id) throws HttpClientErrorException.NotFound {
+    public EntityModel<User> retrieveUserById(@PathVariable int id) throws HttpClientErrorException.NotFound {
         User user = userDaoService.findOne(id);
         if (user == null) {
             throw new UserNotFoundException("nothing ");
-
         }
-        return user;
+        EntityModel<User> model = EntityModel.of(user);
+        WebMvcLinkBuilder linkToUsers = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkToUsers.withRel("all-users"));
+        return model;
     }
 
     @DeleteMapping("/delete/{id}")
     public void deleteUser(@PathVariable("id") int id) {
         User user = userDaoService.deleteById(id);
 
-        System.out.println("---------" + user);
 
         if (user == null) {
             throw new UserNotFoundException("Not Found");
@@ -53,13 +59,15 @@ public class UserController extends CustomizeExceptionHandler {
     public ResponseEntity<Object> saveUser(@Valid @RequestBody User user) {
         User savedUser = userDaoService.save(user);
 
-        //  ServletUriComponentsBuilder --> create links based on the current HttpServletRequest
-        // .fromCurrentRequest()--> Prepare a builder from the host, port, scheme, and context path of the given
-        // .path() --> Append to the path of this builder
-        // buildAndExpand() --> Build a UriComponents instance and replaces URI template variables with the values from an array
+
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
 
         return ResponseEntity.created(location).build();
         //  return new ResponseEntity<>("CREATED", HttpStatus.CREATED);
+
+        //  ServletUriComponentsBuilder --> create links based on the current HttpServletRequest
+        // .fromCurrentRequest()--> Prepare a builder from the host, port, scheme, and context path of the given
+        // .path() --> Append to the path of this builder
+        // buildAndExpand() --> Build a UriComponents instance and replaces URI template variables with the values from an array
     }
 }
